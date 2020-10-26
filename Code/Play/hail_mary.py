80915697dd95f097b286a2d7bbe6bdf65167641e
shane <shaneacton745.sa@gmail.com>
2020-10-27 01:06:16 +0200

    test logic

--- Code/Play/hail_mary.py
+++ Code/Play/hail_mary.py
@@ -5,7 +5,10 @@ import sys
 
 import nlp
 import torch
+from torch.utils.data import DataLoader
 from transformers import LongformerTokenizerFast, Trainer, LongformerForQuestionAnswering, TrainingArguments
+
+from Code.Training.eval_utils import evaluate
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path_1 = os.path.split(os.path.split(dir_path)[0])[0]
@@ -56,7 +59,7 @@ valid_dataset = torch.load(VALID)
 print('loading done')
 
 train_args = TrainingArguments(OUT)
-train_args.per_device_train_batch_size = 1
+train_args.per_device_train_batch_size = 8
 train_args.do_eval = True
 train_args.evaluation_strategy = "steps"
 train_args.eval_steps = 2000
@@ -98,5 +101,28 @@ print("checkpoint:", check)
 trainer.train(model_path=check)
 trainer.save_model()
 
-# model = model.cuda()
-# model.eval()
+model = model.cuda()
+model.eval()
+
+dataloader = DataLoader(valid_dataset, batch_size=16)
+
+answers = []
+with torch.no_grad():
+    for batch in nlp.tqdm(dataloader):
+        start_scores, end_scores = model(input_ids=batch['input_ids'].cuda(),
+                                      attention_mask=batch['attention_mask'].cuda())
+        for i in range(start_scores.shape[0]):
+            all_tokens = tokenizer.convert_ids_to_tokens(batch['input_ids'][i])
+            answer = ' '.join(all_tokens[torch.argmax(start_scores[i]) : torch.argmax(end_scores[i])+1])
+            ans_ids = tokenizer.convert_tokens_to_ids(answer.split())
+            answer = tokenizer.decode(ans_ids)
+            answers.append(answer)
+
+
+predictions = []
+references = []
+for ref, pred in zip(valid_dataset, answers):
+    predictions.append(pred)
+    references.append(ref['answers']['text'])
+
+evaluate(references, predictions)
