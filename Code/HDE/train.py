011edc3cdfde520ec74f2285ff09501437705cb3
shane <shaneacton745.sa@gmail.com>
2021-01-25 13:46:57 +0200

    eval code

--- Code/HDE/train.py
+++ Code/HDE/train.py
@@ -4,6 +4,8 @@ import time
 
 from tqdm import tqdm
 
+from Code.Training.Utils.eval_utils import get_acc_and_f1
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path_1 = os.path.split(os.path.split(dir_path)[0])[0]
 sys.path.append(dir_path_1)
@@ -21,27 +23,38 @@ from Code.Training.Utils.dataset_utils import load_unprocessed_dataset
 
 NUM_EPOCHS = 2
 PRINT_LOSS_EVERY = 50
+MAX_EXAMPLES = 100
 
 print("loading data")
 
-wikihop = load_unprocessed_dataset("qangaroo", "wikihop", nlp.Split.TRAIN)
+train = load_unprocessed_dataset("qangaroo", "wikihop", nlp.Split.TRAIN)
+test = load_unprocessed_dataset("qangaroo", "wikihop", nlp.Split.VALIDATION)
 
 hde = HDEGloveEmbed().to(device)
 optimizer = optim.SGD(hde.parameters(), lr=0.001, momentum=0.9)
 
 losses = []
 last_print = time.time()
-print("num examples:", len(wikihop))
+print("num examples:", len(train))
 for epoch in range(NUM_EPOCHS):
-    for i, example in tqdm(enumerate(wikihop)):
+    answers = []
+    predictions = []
+    for i, example in tqdm(enumerate(train)):
         optimizer.zero_grad()
+        if i >= MAX_EXAMPLES:
+            break
+
         # print(example)
         answer = example["answer"]
         candidates = example["candidates"]
         query = example["query"]
         supports = example["supports"]
 
-        loss, ans = hde(supports, query, candidates, answer=answer)
+        loss, predicted = hde(supports, query, candidates, answer=answer)
+
+        answers.append([answer])
+        predictions.append(predicted)
+
         t = time.time()
         loss.backward()
         if sysconf.print_times:
@@ -53,3 +66,5 @@ for epoch in range(NUM_EPOCHS):
         if len(losses) % PRINT_LOSS_EVERY == 0:
             print("e", epoch, "i", i, "loss:", mean(losses[-PRINT_LOSS_EVERY:-1]), "mean:", mean(losses), "time:", (time.time() - last_print))
             last_print = time.time()
+
+    print("e", epoch, "completed", get_acc_and_f1(answers, predictions))
