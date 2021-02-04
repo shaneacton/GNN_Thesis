194d11d0213232b40e414d77e21b95e6acadbbb4
shane <shaneacton745.sa@gmail.com>
2021-02-04 22:42:54 +0200

    path

--- Code/HDE/scheduler.py
+++ Code/HDE/scheduler.py
@@ -3,14 +3,13 @@ import pathlib
 import sys
 from os.path import join
 
-from Code.HDE.Config import load_configs, load_checkpoint_model_config
-from Code.HDE.trainer import train_model
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path_1 = os.path.split(os.path.split(dir_path)[0])[0]
 sys.path.append(dir_path_1)
 sys.path.append(os.path.join(dir_path_1, 'Code'))
 
+from Code.HDE.Config import load_configs, load_checkpoint_model_config
+from Code.HDE.trainer import train_model
 
 file_path = pathlib.Path(__file__).parent.absolute()
 CHECKPOINT_FOLDER = join(file_path, "Checkpoint")
