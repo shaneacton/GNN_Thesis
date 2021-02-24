39d7ec26fc6cc66eab1d8dd7de263b3ba9d4f03d
shane <shaneacton745.sa@gmail.com>
2021-02-24 23:26:05 +0200

    .

--- Code/HDE/wikipoint.py
+++ Code/HDE/wikipoint.py
@@ -1,15 +1,15 @@
 from typing import Tuple, List
 
-from Config import config
 from Code.Embedding.Glove.glove_utils import get_glove_entity_token_spans
 from Code.HDE.Graph.graph_utils import get_transformer_entity_token_spans
+from Config.config import conf
 
 
 class Wikipoint:
 
     def __init__(self, example, glove_embedder=None, tokeniser=None):
         supports = example["supports"]
-        supports = [s[:config.max_context_chars] if config.max_context_chars != -1 else s for s in supports]
+        supports = [s[:conf.max_context_chars] if conf.max_context_chars != -1 else s for s in supports]
 
         if glove_embedder is not None:
             self.ent_token_spans: List[List[Tuple[int]]] = get_glove_entity_token_spans(supports, glove_embedder)
