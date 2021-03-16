34d7e98b18a985b7f811eef03e08f43518cd9dc6
shane <shaneacton745.sa@gmail.com>
2021-03-16 20:04:53 +0200

    added simple gnn. updated scheduler

--- Code/GNNs/gnn_stack.py
+++ Code/GNNs/gnn_stack.py
@@ -22,7 +22,10 @@ class GNNStack(nn.Module):
                 layer_kwargs.update({"dropout": conf.dropout})
         for layer_i in range(conf.num_layers):
             in_size = conf.embedded_dims if layer_i == 0 else conf.hidden_size
-            layer = GNNLayer(GNNClass, in_size, **layer_kwargs)
+            if conf.use_simple_gnn:
+                layer = SimpleGNNLayer(GNNClass, in_size, **layer_kwargs)
+            else:
+                layer = GNNLayer(GNNClass, in_size, **layer_kwargs)
             if use_gating:
                 layer = GatedGNN(layer)
             layers.append(layer)
@@ -79,26 +82,9 @@ class SimpleGNNLayer(nn.Module):
             self.gnn = GNNClass(in_channels, conf.hidden_size//layer_kwargs["heads"], **layer_kwargs)
         else:
             self.gnn = GNNClass(in_channels, conf.hidden_size, **layer_kwargs)
-
-        self.linear1 = Linear(conf.hidden_size, conf.hidden_size * intermediate_fac)
-        self.dropout = Dropout(conf.dropout)
-        self.linear2 = Linear(conf.hidden_size * intermediate_fac, conf.hidden_size)
-
-        self.norm1 = LayerNorm(conf.hidden_size)
-        self.norm2 = LayerNorm(conf.hidden_size)
         self.dropout1 = Dropout(conf.dropout)
-        self.dropout2 = Dropout(conf.dropout)
 
     def forward(self, x, **kwargs):
         "x ~ (N, in_channels)"
-        x2 = self.dropout1(self.gnn(x, **kwargs))  # # (N, out_channels)
-        if x.size(-1) == x2.size(-1):
-            x = x + x2  # residual
-            x = self.norm1(x)
-        else:  # no residual if this layer is changing the model dim
-            x = self.norm1(x2)
-        x2 = self.linear2(self.dropout(torch.relu(self.linear1(x))))
-        x = x + self.dropout2(x2)  # residual
-        x = self.norm2(x)
-
+        x = self.dropout1(torch.relu(self.gnn(x, **kwargs)))  # # (N, out_channels)
         return x
