2dcc00525d6de2d7c89ffaadd9cc7ccc3ef1e476
shane <shaneacton745.sa@gmail.com>
2021-04-15 21:16:29 +0200

    .

--- Code/Main/scheduler.py
+++ Code/Main/scheduler.py
@@ -91,6 +91,7 @@ def get_next_model_config(debug, repeat_num=0):
                 model_names[c] = effective_name(load_effective_config(c, "base")["model_name"], repeat_num)
                 valid_confs.append(c)
             except Exception as e:
+                print("error loading conf:", c)
                 print(e)
         started = [conf for conf in valid_confs if exists(get_model_checkpoint_folder(model_names[conf]))]
         not_started = [conf for conf in valid_confs if conf not in started]
