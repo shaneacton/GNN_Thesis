9fc77600c1b03d250e901afd47e40e4c7ecbb231
shaneacton <shaneacton745.sa@gmail.com>
2021-09-06 17:30:58 +0200

    schedj

--- Code/GNNs/wrap_gnn.py
+++ Code/GNNs/wrap_gnn.py
@@ -130,8 +130,16 @@ if __name__ == "__main__":
     edge_gnn = EdgeEmbeddings(base_gnn, hid, num_edge_types)
 
     x = torch.zeros(num_nodes, hid)
-    edge = torch.tensor([[0,1,2,3], [1,0,3,2]]).long()
-    edge_types = torch.tensor([0, 0, 1, 1] + [2] * num_nodes).long()
+    print("nodes:", x.size())
+    edge_index = torch.tensor([[0,1,2,3], [1,0,3,2]]).long()  # these are the edges we want added
+    """here edges is an (E, 2) list defined as [[from_ids][to_ids]]"""
+    print("edges:", edge_index.size())
+    """
+        A self edge connects a node to itself
+        the GAT adds in self edges automatically. these new edges are always appended to the end of the edge index
+        thus we need a special edge type for self edges. Here we use edgetype=2 for self edges
+    """
+    edge_types = torch.tensor([0, 0, 1, 1] + [2] * num_nodes).long()  # will initially mismatch in size compared to edge_index
 
-    x_out = edge_gnn(x, edge, edge_types=edge_types, return_attention_weights=None)
+    x_out = edge_gnn(x, edge_index, edge_types=edge_types, return_attention_weights=None)
 
