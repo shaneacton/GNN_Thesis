9ef75cc17178002e4e96290905a30c15d8a8bcb0
shaneacton <shaneacton745.sa@gmail.com>
2022-01-23 20:35:22 +0200

    bug

--- Code/GNNs/linear_gnn.py
+++ Code/GNNs/linear_gnn.py
@@ -1,12 +1,18 @@
 from torch import nn, Tensor
 from torch_geometric.nn import MessagePassing
 
+from Code.HDE.Graph.graph import HDEGraph
+
 
 class LinearGNN(MessagePassing):
 
     def __init__(self, in_size, out_size):
         super().__init__(aggr="mean")
         self.message_transform = nn.Linear(in_size, out_size)
+
+    def forward(self, x, edge_index):
+        out = self.propagate(edge_index, x=x)
+        return out
 
     def message(self, x_j: Tensor) -> Tensor:
         return self.message_transform(x_j)
