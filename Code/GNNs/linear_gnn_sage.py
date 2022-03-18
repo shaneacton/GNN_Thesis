538c619edea0bd43826a4346b4ffeb94076638a8
shaneacton <shaneacton745.sa@gmail.com>
2022-03-18 19:25:48 +0200

    schedj

---
+++ Code/GNNs/linear_gnn_sage.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn, Tensor
+from torch_geometric.nn import MessagePassing
+
+
+class LinearGNNSAGE(MessagePassing):
+
+    def __init__(self, in_size, out_size):
+        super().__init__(aggr="mean")
+        self.message_transform = nn.Linear(in_size, out_size)
+        self.concat_transform = nn.Linear(in_size * 2, out_size)
+
+    def forward(self, x, edge_index):
+        out = self.propagate(edge_index, x=x)
+        return out
+
+    def message(self, x_j: Tensor) -> Tensor:
+        return self.message_transform(x_j)
+
+    def update(self, agg_msg: Tensor, x: Tensor) -> Tensor:
+        # print("linsage update. agg:", agg_msg.size(), "x:", x.size())
+        concat = torch.cat([x, agg_msg], dim=-1)
+        return agg_msg + self.concat_transform(concat)
--- Code/Utils/model_utils.py
+++ Code/Utils/model_utils.py
@@ -11,6 +11,7 @@ from Code.GNNs.TransGNNs.transformer_gnn_edge import TransformerGNNEdge
 from Code.GNNs.linear_gnn import LinearGNN
 from Code.GNNs.linear_gnn2 import LinearGNN2
 from Code.GNNs.linear_gnn3 import LinearGNN3
+from Code.GNNs.linear_gnn_sage import LinearGNNSAGE
 from Code.HDE.hde_model import HDEModel
 from Code.HDE.hde_rel import HDERel
 from Code.Training import dev
@@ -26,7 +27,7 @@ MODEL_MAP = {
              }
 
 GNN_MAP = {"GATConv": GATConv, "SAGEConv": SAGEConv, "TransformerEdge": TransformerGNNEdge, "Linear": LinearGNN,
-           "Linear2": LinearGNN2, "Linear3": LinearGNN3}
+           "Linear2": LinearGNN2, "Linear3": LinearGNN3, "LinearSAGE": LinearGNNSAGE}
 
 
 def get_model(name, MODEL_CLASS=None, **model_kwargs):
