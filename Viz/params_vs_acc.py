bd754bcb162ce3a19496b0f37c09fbe44febdbb3
shaneacton <shaneacton745.sa@gmail.com>
2022-04-27 21:26:21 +0200

    schedj

--- Viz/params_vs_acc.py
+++ Viz/params_vs_acc.py
@@ -1,11 +1,16 @@
 # GROUP_NAME = "MLP-Base"
-# ACCURACIES = [60, 58.9, 59.6, 57.6, 62]  # base_0, base_0_switch, base_0_sage, *base_0_gru, base_0_NoShare
+# # base_0, base_0_switch, base_0_sage, *base_0_gru, base_0_NoShare
+# ACCURACIES = [60, 58.9, 59.6, 57.6, 62]
 # NUM_PARAMS = [3.2, 3.7, 3.3, 8.3, 6.1]
 
-# # base1, base1_SDP, base1_sage, base1_linear, base1_nogate, base1_noTuf, base1_noSS, base1_share
+# # base0 new, new no gate, new tuf, new tuf no gate, newnew no gate, newnew, new sage, new sage no gate
+# ACCURACIES = [60.5, 52.7, 61.5, 64.1, 58.2, 60.3, 61.9, *]
+# NUM_PARAMS = [3.2, 2.9, 3.6, 3.3, 3.0, 3.3, 3.4, *]
+
 from typing import List
 
 GROUP_NAME = "Base1"
+# base1, base1_SDP, base1_sage, base1_linear, base1_nogate, base1_noTuf, base1_noSS, base1_share
 ACCURACIES = [64, 64.7, 57.3, 60.1, 60.3, 62.9, 65, 63.6]
 NUM_PARAMS = [10.8, 13.2, 11.6, 10.8, 8.4, 7.5, 6.1, 5.0]
 
